49a9ef9fac63e94e9bceac1ba6746f21fa6d6350
Amir-Moris <83420413+Amir-Moris@users.noreply.github.com>
2021-09-06 02:13:43 +0200

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fre freopen("input.txt","r",stdin) , freopen("output.txt","w",stdout) ;
+#define ll long long
+using namespace std;
+const int N=1e5+5 ;
+const int mod=1e9+7 ;
+
+int main() {
+    IO
+
+}
