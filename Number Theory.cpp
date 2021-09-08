c518ada05ee08ea70f9685f0146e9bc65af0f493
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2021-09-08 21:43:58 +0200

    Update Number Theory.cpp

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -35,7 +35,7 @@ int fac[N] ;
 void Factorial() {
     fac[0]=1 ;
     for(int i=1; i<N; i++)
-        fac[i]=i*fac[i-1] %mod ;
+        fac[i]=1ll*i*fac[i-1] %mod ;
 }
 long long comb(int n, int r) { // fp is fast powert
     return 1ll*fac[n] * fp(1ll*fac[r]*fac[n-r]%mod,mod-2) %mod ;
