04dd05b813f49202598acafdb59411ace79ce374
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2021-09-28 23:06:06 +0200

    Update Data_Structures.cpp

--- Data_Structures.cpp
+++ Data_Structures.cpp
@@ -1,7 +1,12 @@
+ordered_set (dont`t forget to change data-type)
+// dont`t forget to change data-type !!
+// find_by_order(k) [iterator] {random access}
+// order_of_key(k) [int] {number of items < k}
+  
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 #define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
 
-// ****************************************************************************************************** //
+// ************************************************************************************************************** //
