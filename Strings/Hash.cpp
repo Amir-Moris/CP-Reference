9fd17d55ea830213ba995a202929093bb40898f1
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-03-07 15:40:44 +0200

    Update Hash.cpp

--- Strings/Hash.cpp
+++ Strings/Hash.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int N=1e5+5, p1=31, p2=37, mod=1e9+7 ;
+const int N=1e5+5, P1=31, P2=37, mod=1e9+7 ;
 int add(int a, int b) {
-    a = ((a%mod) + mod) %mod, b = ((b%mod) + mod) %mod ;
+    a = ((a % mod) + mod) % mod, b = ((b % mod) + mod) % mod ;
     return (a + b) %mod ;
 }
 int mul(int a, int b) {
-    a = ((a%mod) + mod) %mod, b = ((b%mod) + mod) %mod ;
+    a = ((a % mod) + mod) % mod, b = ((b % mod) + mod) % mod ;
     return (a * 1LL * b) %mod ;
 }
 int fp(int b, int p) {
@@ -16,20 +16,19 @@ int fp(int b, int p) {
     if(p%2) return ans * 1LL * b %mod;
     return ans%mod;
 }
-int modinv(int b,int mod) {
-    return fp(b,mod-2) ;
+int modinv(int b, int mod) {
+    return fp(b, mod - 2) ;
 }
 int power1[N], inv1[N], power2[N], inv2[N] ;
-void prePow() {
-    power1[0] = inv1[0] = 1;
-    power2[0] = inv2[0] = 1;
-    int inverse1 = modinv(p1,mod-2), inverse2 = modinv(p2,mod-2) ;
+void PrePower() {
+    power1[0] = inv1[0] = power2[0] = inv2[0] = 1;
+    int ModInverse1 = modinv(P1, mod);
+    int ModInverse2 = modinv(P2, mod);
     for(int i=1; i<N; i++) {
-        power1[i] = mul(p1,power1[i-1]) ;
-        inv1[i] = mul(inverse1,inv1[i-1]) ;
-
-        power2[i] = mul(p2,power2[i-1]) ;
-        inv2[i] = mul(inverse2,inv2[i-1]) ;
+        power1[i] = mul(P1, power1[i - 1]) ;
+        power2[i] = mul(P2, power2[i - 1]) ;
+        inv1[i] = mul(ModInverse1, inv1[i - 1]) ;
+        inv2[i] = mul(ModInverse2, inv2[i - 1]) ;
     }
 }
 struct Hash {
@@ -55,12 +54,12 @@ struct Hash {
     pair<int,int> getHashRange(int l, int r) {
         int First = add(prefixHash[r].first, -(l ? prefixHash[l - 1].first : 0)) ;
         int Second = add(prefixHash[r].second, -(l ? prefixHash[l - 1].second : 0)) ;
-        First = mul(First,inv1[l]), Second = mul(Second,inv1[l]) ;
+        First = mul(First,inv1[l]), Second = mul(Second,inv2[l]) ;
 
         return make_pair(First,Second) ;
     }
 };
 int main() {
-    prePow();
+    PrePower();
 
 }
