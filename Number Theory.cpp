012df900ba37f85e962fc13d6c25a2273fd1ec0d
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-03-24 19:17:57 +0200

    Update Number Theory.cpp

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -23,29 +23,29 @@ int eGCD(int a, int b, int& x0, int& y0) { // a*x + b*y = g
 
 int fp(int b, int p) {
     if(!p) return 1;
-    int ans = fp(b * 1LL * b %Mod, p/2) %Mod ;
-    if(p%2) return ans * 1LL * b %Mod;
-    return ans%Mod;
+    int ans = fp(b * 1LL * b %MOD, p/2) %MOD ;
+    if(p%2) return ans * 1LL * b %MOD;
+    return ans%MOD;
 }
 // ***************************************************************************************** //
 // ** Modular Inverse ** //
 
-int modinv(int b, int mod) { // mod must be prime
-    return fp(b, mod - 2) ;
+int modinv(int b, int Mod) { // mod must be prime
+    return fp(b, Mod - 2) ;
 }
 // ***************************************************************************************** //
 // ** Combinatorics/nCr ** //
 
-const int N =1e5+5 ;
+const int N=1e5+5, MOD=1e9+7 ;
 int fac[N] ;
 void PreFactorial() {
     fac[0] = 1;
     for(int i=1; i<N; i++)
-        fac[i] = i * 1LL * fac[i-1] %Mod ;
+        fac[i] = i * 1LL * fac[i-1] %MOD ;
 }
 int comb(int n,int r) {
     if(n<r) return 0;
-    return fac[n] * 1LL * fp(fac[r] * 1LL * fac[n-r] %Mod, Mod-2) %Mod ;
+    return fac[n] * 1LL * fp(fac[r] * 1LL * fac[n-r] %MOD, MOD-2) %MOD ;
 }
 // ***************************************************************************************** //
 
