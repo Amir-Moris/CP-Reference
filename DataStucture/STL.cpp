8ad3ae65e1b2d3050082096bf18eac944e73c495
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-04-03 14:46:04 +0200

    Update STL.cpp

--- DataStucture/STL.cpp
+++ DataStucture/STL.cpp
@@ -1,8 +1,8 @@
 // ****** ordered_set ****** //
 // dont`t forget to change data-type !! //
 // ordered_multiset reverse upper_bound & lower_bound !! //
- find_by_order(k) // [iterator] {iterator on the Kth element} //
- order_of_key(k) // [int] {number of items < k} //
+// find_by_order(k) // [iterator] {iterator on the Kth element} //
+// order_of_key(k) // [int] {number of items < k} //
   
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
