27dabdfd31f10534e8bf0783fd21feb39887ead9
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-04-13 21:49:31 +0200

    Update Bitmasks.cpp

--- Bitmasks.cpp
+++ Bitmasks.cpp
@@ -1,12 +1,11 @@
-To compute xor value from 0 to n
-
+// To compute xor value from 0 to n
 int compXOR(int n) {
-    if (n % 4 == 0)
-        return n;
-    if (n % 4 == 1)
-        return 1;
-    if (n % 4 == 2)
-        return n + 1;
+    if (n % 4 == 0)  return n;
+    if (n % 4 == 1) return 1;
+    if (n % 4 == 2) return n + 1;
     return 0;
 }
-// ***************************************************************************************** //
+
+bool mnwr(int mask, int bit) {
+    return ((mask >> bit) & 1);
+}
