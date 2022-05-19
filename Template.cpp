1f0d4db93cff789cfb8e839b69f727adbeb93c73
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-05-19 02:40:04 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #pragma GCC optimize ("O3")
 #define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define fre freopen("input.in","r",stdin) , freopen("output.out","w",stdout) ;
+#define fre freopen("input.in", "r", stdin); freopen("output.out", "w", stdout);
 #define ll long long
 using namespace std;
 const int N = 1e5+5, MOD = 1e9+7 ;
