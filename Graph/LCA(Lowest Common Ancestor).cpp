bb96c43574e6b097201af2ad0008174f0bfa04f2
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-07-12 03:01:30 +0200

    Update LCA(Lowest Common Ancestor).cpp

--- Graph/LCA(Lowest Common Ancestor).cpp
+++ Graph/LCA(Lowest Common Ancestor).cpp
@@ -1,26 +1,26 @@
-const int N = 1e5+5 ;
-int LOG, timer, tin[N], tout[N] ;
-vector<vector<int>> adj, up ;
+const int N = 1e5 + 5;
+vector<vector<int>> adj, up;
+int LOG, timer, tin[N], tout[N];
 
 void DFS(int node, int parent) {
-    tin[node] = timer++ ;
-    up[node][0] = parent ;
+    tin[node] = timer++;
+    up[node][0] = parent;
 
     for(int bit = 1; bit < LOG; bit++) {
-        int prev = up[node][bit - 1] ;
-        up[node][bit] = up[prev][bit - 1] ;
+        int prev = up[node][bit - 1];
+        up[node][bit] = up[prev][bit - 1];
     }
 
     for(auto child : adj[node]) {
         if(child != parent) {
-            DFS(child,node) ;
+            DFS(child,node);
         }
     }
-    tout[node] = timer++ ;
+    tout[node] = timer++;
 }
 
 bool isAncestor(int x, int y) {
-    return (tin[x] <= tin[y] && tout[x] >= tout[y]) ;
+    return (tin[x] <= tin[y] && tout[x] >= tout[y]);
 }
 
 int LCA(int x, int y) {
@@ -29,7 +29,7 @@ int LCA(int x, int y) {
 
     for(int k = LOG - 1; k >= 0; k--) {
         if(!isAncestor(up[x][k],y)) {
-            x = up[x][k] ;
+            x = up[x][k];
         }
     }
     return up[x][0];
@@ -38,8 +38,8 @@ int LCA(int x, int y) {
 void zpt(int sz) {
     timer = 0;
     LOG = log2(sz) + 1;
-    adj = vector<vector<int>> (sz + 5, vector<int>()) ;
-    up = vector<vector<int>> (sz + 5, vector<int>(LOG + 5)) ;
+    adj = vector<vector<int>> (sz + 5, vector<int>());
+    up = vector<vector<int>> (sz + 5, vector<int>(LOG + 5));
 }
 
 void BuildTree() {
