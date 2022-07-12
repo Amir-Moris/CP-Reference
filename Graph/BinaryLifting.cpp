03f25f6d7d52773ab2c6c5ebb2803e8071c67ee7
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-07-12 03:16:34 +0200

    Update BinaryLifting.cpp

--- Graph/BinaryLifting.cpp
+++ Graph/BinaryLifting.cpp
@@ -1,19 +1,19 @@
-const int N = 1e5+5 ;
-int LOG, depth[N] ;
-vector<vector<int>> adj, up ;
+const int N = 1e5 + 5;
+vector<vector<int>> adj, up;
+int LOG, depth[N];
 
 void DFS(int node, int parent) {
-    up[node][0] = parent ;
-    depth[node] = depth[parent] + 1 ;
+    up[node][0] = parent;
+    depth[node] = depth[parent] + 1;
 
     for(int bit = 1; bit < LOG; bit++) {
-        int prev = up[node][bit - 1] ;
-        up[node][bit] = up[prev][bit - 1] ;
+        int prev = up[node][bit - 1];
+        up[node][bit] = up[prev][bit - 1];
     }
 
     for(auto child : adj[node]) {
         if(child != parent) {
-            DFS(child,node) ;
+            DFS(child, node);
         }
     }
 
@@ -27,14 +27,15 @@ int getKthAncestor(int node, int k) {
     if(k > depth[node]) return 0;
 
     for(int j = LOG - 1; j >= 0; j--) {
-        if(mnwr(k,j))
+        if(mnwr(k, j)) {
             node = up[node][j];
+        }
     }
     return node;
 }
 
 void zpt(int sz) {
     LOG = log2(sz) + 1;
-    adj = vector<vector<int>> (sz + 5, vector<int>()) ;
-    up = vector<vector<int>> (sz + 5, vector<int>(LOG + 5)) ;
+    adj = vector<vector<int>> (sz + 5, vector<int>());
+    up = vector<vector<int>> (sz + 5, vector<int>(LOG + 5));
 }
