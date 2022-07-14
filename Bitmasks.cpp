e451165f9c5d60cc5f8ce0d75d91915e746373fa
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-07-14 19:37:27 +0200

    Update Bitmasks.cpp

--- Bitmasks.cpp
+++ Bitmasks.cpp
@@ -16,4 +16,31 @@ bool mnwr(int mask, int bit) {
     return ((mask >> bit) & 1);
 }
 
+int nwr(int mask, int bit) {
+    return mask | (1LL << bit);
+}
+
+int dlm(int mask, int bit) {
+    return mask & ~(1LL << bit);
+}
+
+int toggleBit(int mask, int bit) {
+    return mask ^ (1LL << bit);
+}
+
+int leastSignificantBit(int mask) {
+    return mask & (-mask);
+}
+int mostSignificantBit(int mask) {
+    for(int i = 31; i >= 0; i--) {
+        if(mnwr(mask, i)) {
+            return (1LL << i);
+        }
+    }
+    return 0;
+}
+
+bool isPowerOfTwo(int mask) {
+    return !(mask & (mask - 1));
+}
 
