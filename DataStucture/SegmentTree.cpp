de3fdaa27d8ffbbe4fc6ab026fcac2c260c75feb
Amir Moris <83420413+Amir-Moris@users.noreply.github.com>
2022-07-23 18:10:31 +0200

    Update SegmentTree.cpp

--- DataStucture/SegmentTree.cpp
+++ DataStucture/SegmentTree.cpp
@@ -2,35 +2,36 @@ template<class T> struct segtree {
     int sz = 1;
     vector<T> sums;
     void init(int n) {
-        sz = 1;
-        while(sz < n) sz *= 2;
-        sums = vector<T> (2 * sz, 0);
+        int tmp = 1;
+        sz = n;
+        while(tmp < n) tmp *= 2;
+        sums = vector<T> (2 * tmp, 0);
     }
-
-    void update(int pos, int val, int cur, int l, int r) {
-        if(r - l == 1) {
-            sums[cur] = val;
+    void update(int cur, int l, int r, int pos, int value) {
+        if(l == r) {
+            sums[cur] = value;
             return;
         }
         int mid = (l + r) / 2;
-        if(pos < mid) {
-            update(pos, val, 2 * cur + 1, l, mid);
-        } else {
-            update(pos, val, 2 * cur + 2, mid, r);
-        }
+        if(pos <= mid)
+            update(2 * cur + 1, l, mid, pos, value);
+        else
+            update(2 * cur + 2, mid + 1, r, pos, value);
+
         sums[cur] = sums[2 * cur + 1] + sums[2 * cur + 2];
     }
-    void update(int pos, int val) {
-        update(pos, val, 0, 0, sz);
+    void update(int pos, int value) {
+        update(0, 0, sz, pos, value);
     }
 
-    T getSum(int lx, int rx, int cur, int l, int r) {
-        if(l >= rx || r <= lx) return 0;
+    T getSum(int cur, int l, int r, int lx, int rx) {
+        if(l > rx || r < lx) return 0;
         if(l >= lx && r <= rx) return sums[cur];
         int mid = (l + r) / 2;
-        return getSum(lx, rx, 2 * cur + 1, l, mid) + getSum(lx, rx, 2 * cur + 2, mid, r);
+        return getSum(2 * cur + 1, l, mid, lx, rx)
+               + getSum(2 * cur + 2, mid + 1, r, lx, rx);
     }
-    T getSum(int l, int r) {
-        return getSum(l, r, 0, 0, sz);
+    T getSum(int lx, int rx) {
+        return getSum(0, 0, sz, lx, rx);
     }
 };
